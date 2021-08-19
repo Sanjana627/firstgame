d387bc76d9e6ef80b4640e2062763baa6fb6e238
Sanjana627 <sanjanasaini627@gmail.com>
2021-08-19 12:30:44 +0530

    initial commit

---
+++ quizgame.py
@@ -0,0 +1,42 @@
+print("welcome to my computer quiz ! ")
+
+playing=input(" Do you want to play? ")
+if playing.lower() != "yes":
+    quit()
+
+print("Okay ! Let's play: ")
+score=0
+answer=input("what does CPU stands for? ")
+
+if answer.lower()=="central processing unit":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+
+answer = input("what does GPU stands for? ")
+
+if answer.lower() == "graphic processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("what does RAM stands for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("what does PSU stands for? ")
+
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got"+str(score)+"questions correct!")
+print("you got"+str((score/4)*100)+"%.")
